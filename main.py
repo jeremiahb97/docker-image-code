46afc65708b78b60f50f8e9e81d5d0ff5c4441a9
jeremiahb97 <jeremy.barton97@gmail.com>
2025-06-06 18:59:27 -0500

    main.py

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def hello():
     return "It works!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # ✅ dynamic and Cloud Run–friendly
+    port = int(os.environ.get("PORT", 8081)) # ✅ dynamic and Cloud Run–friendly
     app.run(host="0.0.0.0", port=port)
