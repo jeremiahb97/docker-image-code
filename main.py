8f7ef1a2db24408769282a1196d366f5f5419818
jeremiahb97 <jeremy.barton97@gmail.com>
2025-06-06 18:57:31 -0500

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GCP!"
+    return "It works!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))  # ✅ dynamic and Cloud Run–friendly
+    app.run(host="0.0.0.0", port=port)
